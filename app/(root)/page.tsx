a6062aca67af80f089926bea86164c3e593c9792
Robert Walker <robertWalker6580@gmail.com>
2025-05-15 13:11:58 -0700

    add info boxes to homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import { FaTicket } from "react-icons/fa6";
+import { FaBoxes } from "react-icons/fa";
+import { MdLocalShipping } from "react-icons/md";
+import { FaUser } from "react-icons/fa6";
 
 const Home = () => {
   return (
-    <div className="bg-gray-100 py-10 mt-20">
+  <>
+      <section className="bg-gray-100 py-10 mt-20">
       <div className="wrapper">
         <div className="flex flex-col md:flex-row gap-8 items-center">
           <div className="w-full md:w-1/2">
@@ -18,7 +24,70 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
+    <section>
+      <div className="wrapper">
+        <h2 className="text-3xl font-light my-20">Why Use SupporHub</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-20">
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2">
+                <FaTicket className="text-cyan-600" size={40} />
+                <h3>Reduce support response times</h3>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Streamline your support workflow with automated ticket routing and real-time notifications, ensuring customer inquiries are addressed within minutes, not hours.
+              </p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2">
+                <FaBoxes className="text-cyan-600" size={40} />
+                <h3>Improve supply chain transparency</h3>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Gain real-time visibility into your supply chain operations, from order placement to delivery, empowering you to make informed decisions and optimize your logistics processes.
+              </p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2">
+                <MdLocalShipping className="text-cyan-600" size={43} />
+                <h3>Minimize shipping delays and issues</h3>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Track shipments in real-time, identify potential delays, and proactively address issues before they impact your customers.
+              </p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2">
+                <FaUser className="text-cyan-600" size={26} />
+                <h3>Enhance customer satisfaction</h3>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Provide customers with a seamless and efficient support experience, ensuring their needs are met promptly and effectively.
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+        
+      </div>
+    </section>
+  </>
+
   );
 }
 
